963f373b5d84f5f63be25275638cd15f46e38a3e
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-02-09 04:49:05 -0500

    Se termino el registro de tareas

--- src/app/tarea/tarea/registro/registro.component.ts
+++ src/app/tarea/tarea/registro/registro.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import { InfoProyectosService } from '../../../services/info-proyectos.service';
 import data from '../../../../assets/proyectos.json';
 
-// import custom validator to validate that password and confirm password fields match
-import { MustMatch } from '../../../_helpers/must-match.validator';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -14,42 +11,50 @@ export class RegistroComponent implements OnInit {
 
   registerForm: FormGroup;
   submitted = false;
-  proyectos : any[];
-
-  constructor(private formBuilder: FormBuilder /*, private _InfoProyectosService: InfoProyectosService*/) { }
+  proyectos : any;
+  tareas : any[];
+  constructor(private formBuilder: FormBuilder) {
+    this.proyectos =  data;
+  }
 
   ngOnInit() {
     this.registerForm = this.createFormGroup();
-    this.getProyectos();
   }
 
   createFormGroup() {
     return this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
-    }, {
-      validator: MustMatch('password', 'confirmPassword')
+      nombreTa: ['', Validators.required],
+      nombrePro: ['', Validators.required],
+      horas: ['', Validators.required]
     });
 
   }
-  // convenience getter for easy access to form fields
   get f() { return this.registerForm.controls; }
 
   onSubmit() {
     this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-      return;
+    this.tareas = [];
+    if (localStorage.length > 0)
+    {
+            this.tareas = JSON.parse(localStorage.getItem("tareas"));
+            this.tareas.push(this.registerForm.value);
+            if (typeof(Storage) !== "undefined") {
+              localStorage.setItem("tareas", JSON.stringify(this.tareas));
+            }
+            else {
+              alert("browser doesn't support local storage!");
+            }
+            if (this.registerForm.invalid) {
+              return;
+            }
+    }
+    else{
+      this.tareas.push(this.registerForm.value);
+      localStorage.setItem("tareas", JSON.stringify(this.tareas));
     }
 
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
-  }
-  getProyectos(){
-    console.log(data);
 
   }
+
+
 }
